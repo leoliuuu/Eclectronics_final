56f1f83d005115fcab8e00f21af11f96c5c21bf3
arianaolson419 <arianaolson419@gmail.com>
2019-05-02 14:17:07 -0400

    Debugging

--- main_board/main_board.ino
+++ main_board/main_board.ino
@@ -8,6 +8,8 @@ const float kMMPerFoot = 304.8;
 const float kFeetPerMile = 5218.0;
 const float kKmPerMile = 1.609;
 
+char incoming_byte;
+
 // Timing Variabels
 unsigned long start_time;
 unsigned long last_time;
@@ -15,7 +17,7 @@ unsigned long new_time;
 
 const unsigned long kTimeout = 5000; // Time since the sensor was last read.
 
-int incoming_byte;
+//int incoming_byte;
 
 float rpm = 60;  // Starting with a reasonable initial value.
 float mph = 0;
@@ -32,19 +34,31 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
-    if (Serial.read() == 1) {
+    incoming_byte = Serial.read();
+    if (incoming_byte == '1') {
       new_time = millis();
       rpm = UpdateRpm(last_time, new_time);
       mph = CalculateMph(rpm);
-      distance = UpdateDistance(last_time, new_time);
+      distance = UpdateDistance(last_time, new_time, mph);
       last_time = new_time;
+      Serial.print("rpm ");
+      Serial.println(rpm);
+      
+      Serial.print("mph ");
+      Serial.println(mph);
+      
+      Serial.print("distance ");
+      Serial.println(distance);
+      
+      Serial.print("time ");
+      Serial.println(last_time - start_time / 1000);
     }
   }
 }
 
 float UpdateRpm(unsigned long last_time, unsigned long new_time) {
   // Calculate the 2-point moving average RPM of the wheel.
-  float rev_per_millis = 1 / (new_time - last_time);
+  float rev_per_millis = 1 / (float)(new_time - last_time);
   float rev_per_minute = rev_per_millis / kMinutesPerMillis;
   return rev_per_minute + rpm / 2;  // Average with the previous reading.
 }
@@ -52,9 +66,10 @@ float UpdateRpm(unsigned long last_time, unsigned long new_time) {
 float CalculateMph(float rpm) {
   // Source: https://itstillruns.com/calculate-wheel-speed-tire-diameter-7445086.html
   float circumference_feet = kWheelCircumference / kMMPerFoot;
-  float revs_per_mile = circumference_feet / kFeetPerMile;
+  float revs_per_mile = kFeetPerMile / circumference_feet;
   float speed_per_min = rpm / revs_per_mile;
-  float speed_per_hour = rpm * 60;
+  float speed_per_hour = speed_per_minute * 60;
+  Serial.print("speed mph"); Serial.println(speed_per_hour);
   return mph + speed_per_hour / 2;  // Average with the previous reading.
 }
 

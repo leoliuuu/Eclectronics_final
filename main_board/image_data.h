e0c3bd33b12ecb66e3fa0be68963546fe54bdd8c
arianaolson419 <arianaolson419@gmail.com>
2019-05-05 21:47:32 -0400

    Add background images

---
+++ main_board/image_data.h
@@ -0,0 +1,13 @@
+namespace imagedata {
+  
+extern const unsigned char background_0[];
+
+extern const unsigned char background_25[];
+
+extern const unsigned char background_50[];
+
+extern const unsigned char background_75[];
+
+extern const unsigned char background_100[];
+
+}   // namespace imagedata
